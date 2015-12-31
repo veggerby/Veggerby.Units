9ea7434e9b3723f9e9aed88866b8084dc874e1de
Jesper Veggerby <jesper@veggerby.com>
2015-12-31 11:00:00 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -29,24 +29,11 @@ dnvm use 1.0.0-rc1-update1
 
 dnu pack src/Veggerby.Units --configuration Release --out artifacts/build/nuget --quiet
 
-dnu publish src/OmniSharp --configuration Release --no-source --out artifacts/build/Veggerby.Units --runtime dnx-mono.1.0.0-rc1-update1
+dnu publish src/Veggerby.Units --configuration Release --no-source --out artifacts/build/Veggerby.Units --runtime dnx-mono.1.0.0-rc1-update1
 
 # work around for kpm bundle returning an exit code 0 on failure
 grep "Build failed" buildlog
 rc=$?; if [[ $rc == 0 ]]; then exit 1; fi
-
-curl -LO http://nuget.org/nuget.exe
-mono nuget.exe install dnx-clr-win-x86 -Version 1.0.0-rc1-update1 -Prerelease -OutputDirectory artifacts/build/Veggerby.Units/approot/packages
-
-if [ ! -d "artifacts/build/omnisharp/approot/packages/dnx-clr-win-x86.1.0.0-rc1-update1" ]; then
-    echo 'ERROR: Can not find dnx-clr-win-x86.1.0.0-rc1-update1 in output exiting!'
-    exit 1
-fi
-
-if [ ! -d "artifacts/build/omnisharp/approot/packages/dnx-mono.1.0.0-rc1-update1" ]; then
-    echo 'ERROR: Can not find dnx-mono.1.0.0-rc1-update1 in output exiting!'
-    exit 1
-fi
 
 tree -if artifacts/build/Veggerby.Units | grep .nupkg | xargs rm
 pushd artifacts/build/Veggerby.Units
